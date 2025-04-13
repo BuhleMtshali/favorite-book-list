dbaa4752ed928a9a8ffb48695f7ba3f8f49b8971
Buhle <mtshalizanobuhle7@gmail.com>
2025-04-13 02:42:41 +0200

    updated the modal and added it's interactivity

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//toggling the responsive navbar
 const toggleElements = document.querySelectorAll('.hidden');
 const toggleMenu = document.querySelector('.menu');
 toggleMenu.addEventListener('click', () => {
@@ -5,4 +6,22 @@ toggleMenu.addEventListener('click', () => {
         console.log(element)
         element.classList.toggle('hidden')
     })
+})
+
+//toggling the modal
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const openModal = document.querySelector('.show-modal');
+
+//opening the modal
+openModal.addEventListener('click', () => {
+    modal.classList.remove('hide-modal');
+    overlay.classList.remove('hide-modal')
+});
+
+//closing the modal
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.add('hide-modal');
+    overlay.classList.add('hide-modal')
 })
